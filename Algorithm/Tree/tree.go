c12b23109c817eba49097d3655b8c668e15eb3fa
LinXiaoYi <874183200@qq.com>
2021-07-20 00:24:55 +0800

    add hole

--- Algorithm/Tree/tree.go
+++ Algorithm/Tree/tree.go
@@ -363,3 +363,25 @@ func insertIntoBST(root *TreeNode, val int) *TreeNode {
 	}
 	return root
 }
+
+//****************************************100. 相同的树****************************************
+func isSameTree(p *TreeNode, q *TreeNode) bool {
+
+	if p == nil && q == nil {
+		return true
+	}
+
+	if p != nil && q != nil {
+		if p.Val != q.Val {
+			return false
+		}
+		left := isSameTree(p.Left, q.Left)
+		right := isSameTree(p.Right, q.Right)
+
+		if left && right {
+			return true
+		}
+	}
+
+	return false
+}
