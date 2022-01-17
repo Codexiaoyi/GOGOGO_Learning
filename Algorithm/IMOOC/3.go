82f9982a42593effec30d1b273a53d450a323cc5
Codexiaoyi <874183200@qq.com>
2022-01-18 00:16:57 +0800

    3

--- Algorithm/IMOOC/3.go
+++ Algorithm/IMOOC/3.go
@@ -28,3 +28,55 @@ func move_zeros() {
 //26
 
 //80
+
+//75
+func sortColors(nums []int) {
+	//[0...zero] [two...len-1]
+	zero, two := -1, len(nums)
+	for i := 0; i < two; {
+		if nums[i] == 1 {
+			i++
+		} else if nums[i] == 2 {
+			two--
+			nums[i], nums[two] = nums[two], nums[i]
+		} else {
+			if nums[i] != 0 {
+				return
+			}
+			zero++
+			nums[i], nums[zero] = nums[zero], nums[i]
+			i++
+		}
+	}
+}
+
+//88
+func merge(nums1 []int, m int, nums2 []int, n int) {
+	if n == 0 {
+		return
+	}
+	if m == 0 {
+		copy(nums1, nums2)
+		return
+	}
+	total := m + n - 1         //1
+	index1, index2 := m-1, n-1 //0,0
+	for {
+		if index1 < 0 || index2 < 0 {
+			break
+		}
+		if nums1[index1] > nums2[index2] {
+			nums1[total] = nums1[index1]
+			index1--
+		} else {
+			nums1[total] = nums2[index2]
+			index2--
+		}
+		total--
+	}
+	if index2 >= 0 {
+		for i := 0; i <= index2; i++ {
+			nums1[i] = nums2[i]
+		}
+	}
+}
