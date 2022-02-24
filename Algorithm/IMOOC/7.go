fc01377d44ac25b80f746f499b860c945315c89b
Codexiaoyi <874183200@qq.com>
2022-02-24 18:13:03 +0800

    u

--- Algorithm/IMOOC/7.go
+++ Algorithm/IMOOC/7.go
@@ -1,5 +1,11 @@
 package imooc
 
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
 //二叉树与递归
 //递归步骤：1.递归终止条件 2.递归过程
 
@@ -10,12 +16,84 @@ package imooc
 
 //101
 //222
+func countNodes(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+
+	left := countNodes(root.Left)
+	right := countNodes(root.Right)
+
+	return left + right + 1
+}
+
 //110
 //112
 //404
 //113
+func pathSum113(root *TreeNode, targetSum int) [][]int {
+	res := make([][]int, 0)
+	if root == nil {
+		return res
+	}
+
+	if root.Left == nil && root.Right == nil && root.Val == targetSum {
+		res = append(res, []int{root.Val})
+		return res
+	}
+
+	left := pathSum113(root.Left, targetSum-root.Val)
+	right := pathSum113(root.Right, targetSum-root.Val)
+
+	for _, road := range left {
+		newRoad := make([]int, 0, len(road)+1)
+		newRoad = append(newRoad, root.Val)
+		newRoad = append(newRoad, road...)
+		res = append(res, newRoad)
+	}
+
+	for _, road := range right {
+		newRoad := make([]int, 0, len(road)+1)
+		newRoad = append(newRoad, root.Val)
+		newRoad = append(newRoad, road...)
+		res = append(res, newRoad)
+	}
+
+	return res
+}
+
 //129
 //437
+func pathSum(root *TreeNode, targetSum int) int {
+	//递归遍历法
+	if root == nil {
+		return 0
+	}
+
+	res := includeRootPathSum(root, targetSum)
+	res += pathSum(root.Left, targetSum) + pathSum(root.Right, targetSum)
+	return res
+
+	//前缀和+
+
+}
+func dfs() {
+
+}
+
+func includeRootPathSum(root *TreeNode, targetSum int) int {
+	if root == nil {
+		return 0
+	}
+	res := 0
+	if root.Val == targetSum {
+		res++
+	}
+
+// 	//包含当前节点
+// 	res += includeRootPathSum(root.Left, targetSum-root.Val) + includeRootPathSum(root.Right, targetSum-root.Val)
+// 	return res
+// }
 
 //二分搜索树
 //每个节点的键值都大于左孩子，每个节点的键值小于右孩子；以左右孩子为根的子树仍为二叉搜索树
