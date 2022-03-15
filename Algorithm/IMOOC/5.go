81d2eaa130b6a2ec35aedc380fe595b9ae894957
Codexiaoyi <874183200@qq.com>
2022-03-16 00:05:59 +0800

    up

--- Algorithm/IMOOC/5.go
+++ Algorithm/IMOOC/5.go
@@ -1,6 +1,22 @@
 package imooc
 
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
 //链表
 //206
+func reverseList(head *ListNode) *ListNode {
+	var pre *ListNode
+	for head != nil {
+		temp := head.Next
+		head.Next = pre
+		pre = head
+		head = temp
+	}
+	return pre
+}
+
 //92
 //83
