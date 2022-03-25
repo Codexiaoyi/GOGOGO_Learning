b4767d68f7874125a2e10ad69483438790ff1144
Codexiaoyi <874183200@qq.com>
2022-03-25 23:09:13 +0800

    u

--- Algorithm/List/list2.go
+++ Algorithm/List/list2.go
@@ -14,3 +14,64 @@ func d83(head *ListNode) *ListNode {
 }
 
 //********************************206*********************************
+
+//********************************203*********************************
+func removeElements(head *ListNode, val int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	font := head
+	back := head.Next
+	for back != nil {
+		if back.Val == val {
+			font.Next = back.Next
+			back.Next = nil
+			back = font.Next
+		} else {
+			font = font.Next
+			back = back.Next
+		}
+	}
+	if head.Val == val {
+		return head.Next
+	}
+	return head
+}
+
+//********************************86*********************************
+func partition(head *ListNode, x int) *ListNode {
+	if head == nil {
+		return nil
+	}
+	dummyNode := &ListNode{Next: head}
+	partitionNode := dummyNode
+	if head.Val < x {
+		for head.Next != nil {
+			//找大于等于x的前一个结点
+			if head.Next.Val >= x {
+				partitionNode = head
+				break
+			}
+			head = head.Next
+		}
+		//所有都比x小
+		if partitionNode == dummyNode {
+			return dummyNode.Next
+		}
+	}
+
+	temp := partitionNode.Next
+	//从分割点后一个节点开始，小于x的都顺延放到分割点后
+	for temp.Next != nil {
+		if temp.Next.Val < x {
+			lessNode := temp.Next
+			temp.Next = temp.Next.Next
+			lessNode.Next = partitionNode.Next
+			partitionNode.Next = lessNode
+			partitionNode = partitionNode.Next
+		} else {
+			temp = temp.Next
+		}
+	}
+	return dummyNode.Next
+}
